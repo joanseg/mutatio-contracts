4c751551988f43c4fdf4a32ea6fdcc6943c40b9e
Lucho <luchesar@gmail.com>
2020-02-28 20:44:22 +0000

    Make the unit tests pass

--- contracts/Mutatio.sol
+++ contracts/Mutatio.sol
@@ -44,36 +44,4 @@ contract Mutatio {
         return  orderId;
     }
 
-
-
-
-
-    // function Lottery() public {
-    //     manager = msg.sender;
-    // }
-
-    // function enter() public payable {
-    //     require(msg.value > .01 ether);
-
-    //     players.push(msg.sender);
-    // }
-
-    // function random() private view returns (uint) {
-    //     return uint(keccak256(block.difficulty, now, players));
-    // }
-
-    // function pickWinner() public restricted {
-    //     uint index = random() % players.length;
-    //     players[index].transfer(address(this).balance);
-    //     players = new address[](0);
-    // }
-
-    // modifier restricted() {
-    //     require(msg.sender == manager);
-    //     _;
-    // }
-
-    // function getPlayers() public view returns (address[] memory) {
-    //     return players;
-    // }
 }
