9d9fe56734ab2c819990421c35dca7ece8561284
Lucho <luchesar@gmail.com>
2020-02-29 15:03:04 +0000

    Add MaybeDai to the deployed contracts

--- contracts/MaybeDai.sol
+++ contracts/MaybeDai.sol
@@ -1,16 +1,15 @@
 pragma solidity ^0.5.0;
 
-import "../node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MaybeDai is StandardToken {
+contract MaybeDai is ERC20 {
   string public name = "maybeDai";
   string public symbol = "MDAI";
   uint8 public decimals = 2;
   uint public INITIAL_SUPPLY = 10000;
 
   function MyTokenFunction() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    emit Transfer(address(0), msg.sender, totalSupply_);
+    _mint(msg.sender, INITIAL_SUPPLY);
+    emit Transfer(address(0), msg.sender, totalSupply());
   }
 }
